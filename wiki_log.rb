12ce186cad3ea86a155bea7d568b7b7c7a653d0c
Ben Hughes <ben@pixelmachine.org>
2008-04-25 15:20:32 -0400

    made system suck less
    * still uglier than Fergie on a rainy day, but its a hack

--- wiki_log.rb
+++ wiki_log.rb
@@ -1,37 +1,73 @@
 require 'rubygems'
 require 'mongrel'
 require 'logger'
+require 'active_record'
+
+$db_name = 'wiki_dot.db'
+$dot_fn = 'wiki_map.tmp.dot'
+$png_fn = 'wiki_map.tmp.png'
+
+if !File.exists? $db_name
+  require 'sqlite3'
+  db = SQLite3::Database.new($db_name)
+  db.execute %q{ 
+    CREATE TABLE lines(
+      id INTEGER PRIMARY KEY,
+      created_at DATETIME,
+      line TEXT);
+    }
+  db.close
+end
+
+
+
+ActiveRecord::Base.establish_connection({ :adapter => 'sqlite3', 
+  :dbfile => $db_name})
+
+class Line < ActiveRecord::Base
+end
+
 
 def stripdot(txt)
   CGI::unescape(txt).gsub(/[^A-Za-z0-9_]/, '_')
 end
 
-$file = File.open("wiki_map.dot", "w")
 
-class HeaderHandler < Mongrel::HttpHandler
-
-
+class WikiLoggerHandler < Mongrel::HttpHandler
   def process(request,response)
     response.start(200) do |head, out|
       head["Content-Type"] = "text/plain"
       vars = Mongrel::HttpRequest.query_parse(request.params['QUERY_STRING'])
-      if vars['exit'] == '1'
-        $file.puts '}'
-        $file.close
-        exit! 
-      elsif vars['single']
-        $file.puts "  #{stripdot vars['single']};"
+
+      if vars['single']
+        Line.new(:line => "#{stripdot vars['single']};").save
       else
-        $file.puts "  #{stripdot vars['from']} -> #{stripdot vars['to']};"
+        Line.new(
+          :line => "#{stripdot vars['from']} -> #{stripdot vars['to']};").save
       end
     end
   end
 end
 
+class WikiMapDisplayHandler < Mongrel::HttpHandler
+  def process(request,response)
+    response.start(200) do |head, out|
+      head["Content-Type"] = "image/png"
+      vars = Mongrel::HttpRequest.query_parse(request.params['QUERY_STRING'])
+      File.open($dot_fn, 'w') do |f|
+        f.write((["digraph wiki_map {"] +
+        Line.find(:all).collect{|l| "  #{l.line}"} + ["}"]).join("\n"))
+      end
+      `dot -Tpng #{$dot_fn} -o #{$png_fn}`
+      out.write File.read($png_fn)
+    end
+  end
+end
+      
 
-$file.puts "digraph wiki_map {"
 server = Mongrel::HttpServer.new("127.0.0.1", "9999")
-server.register("/", HeaderHandler.new)
+server.register("/", WikiLoggerHandler.new)
+server.register("/show", WikiMapDisplayHandler.new)
 server.register("/favicon.ico", Mongrel::Error404Handler.new(""))
 server.run.join
 
